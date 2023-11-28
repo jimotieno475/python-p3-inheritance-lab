4faeb57c9c2487e6d6d1c84f9f4c4919168e2ab1
jim <jim.otieno@student.moringaschool.com>
2023-11-28 22:38:00 +0300

    completed

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 
 from user import User
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
 class Student(User):
-    
-    def learn(self):
+    def __init__(self,first_name,last_name,knowledge=None):
+        super().__init__(first_name,last_name)
+        self.knowledge=[]
+    def learn(self,knowledge):
+        if isinstance(knowledge,str):
+            self.knowledge=knowledge
         pass
+
+john=Student("john","kilo",knowledge)
--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,27 @@ from user import User
 
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
+    def __init__(self,first_name,last_name,knowledge=None):
+        super().__init__(first_name,last_name)
+        self.knowledge=knowledge
 
     def teach(self):
+        if not self.knowledge==0:
+            return False
+        else:
+            random_index=random.radient(0,len(self.knowledge))
+            return self.knowledge[random_index]
         pass
+my_teacher = Teacher(knowledge,"My","Teacher")
